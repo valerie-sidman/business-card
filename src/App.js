77ea0f982a4c7410e3bb03bdb9385c491b92594f
Valerie Sidman <valerie.sidman@gmail.com>
2022-10-21 10:24:23 +0300

    created base for section About

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/header/Header";
 import Hero from "./components/hero/Hero";
+import About from "./components/about/About";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero />
+      <About />
     </div>
   );
 }
---
+++ src/components/about/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './About.css';
+
+export default function About() {
+  return (
+    <React.Fragment>
+      <div className="about">
+        <div className="photo"></div>
+      </div>
+    </React.Fragment>
+  )
+}
