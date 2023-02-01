ee5921f4102435259635d0cd55ea4145d55b762f
Valerie Sidman <valerie.sidman@gmail.com>
2023-02-01 17:37:02 +0300

    changed photo in About; added resume file and link for it; fix About.css

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './About.css';
-import photo from "../../images/future_photo_500x500.png";
+import photo from "../../images/photo_500x500.png";
+import resume from '../../data/resume.pdf';
 
 export default function About() {
   return (
@@ -14,8 +15,9 @@ export default function About() {
             <p className="about-text about-text__subsequent-paragraph">Во время учебы в институте я работала в офисе и была хорошо знакома с делопроизводством. Поэтому, после неудачной стажировки я приняла решение вернуться в эту область. Большая часть работы заключалась в ведении документаоборота и общении с клиентами. Но чувство, что я не на своём месте и способна на большее подтолкнуло меня на кардинальную смену профессии.</p>
             <p className="about-text about-text__subsequent-paragraph">Тема разработки меня привлекла больше всех, несмотря на то, что с ней я была знакома посредственно. И вот уже с первого занятия я влюбилась в эту профессию. Я ещё никогда и нигде не ощущала результаты своей работы так ярко, и в таком масштабе. Но больше всего я люблю и ценю разработку за то, что в ней я наконец-то нашла то “рабочее” творчество, которого мне так не хватало в прошлой отрасли. То сколько способов и подходов может существовать у решения одной задачи, возможность проявить свою индивидуальность и педантичность как в архитектуре проекта, так и в “опрятности” написания самого кода, работа с макетами – всё это приводит меня в восторг и доставляет мне то самое удовольствие от работы, которое я искала.</p>
           </div>
-          <div className="resume-button-container">
-            <button className="resume-button">Скачать резюме</button>
+          <div className="resume-link-container">
+            <a rel="noopener noreferrer" href={resume} target="_blank" className="resume-link" >Открыть резюме</a>
+            {/*<button className="resume-button">Открыть резюме</button>*/}
           </div>
         </div>
       </div>
